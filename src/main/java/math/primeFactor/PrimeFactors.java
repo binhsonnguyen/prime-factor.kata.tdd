3f4fecf961fb4f540fb814e1c1eb0e44fdd7ab4c
edward <son@binhsonnguyen.com>
2020-08-19 12:01:08 +0700

    `generate` method

--- src/main/java/math/primeFactor/PrimeFactors.java
+++ src/main/java/math/primeFactor/PrimeFactors.java
@@ -1,4 +1,10 @@
 package math.primeFactor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrimeFactors {
+    public static List<Integer> generate(int number) {
+        return new ArrayList<Integer>();
+    }
 }
