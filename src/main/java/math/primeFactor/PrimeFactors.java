fc5f6c4aa5803e4db1be63ad7cee0268f010b52c
edward <son@binhsonnguyen.com>
2020-08-19 12:30:04 +0700

    pass testEight

--- src/main/java/math/primeFactor/PrimeFactors.java
+++ src/main/java/math/primeFactor/PrimeFactors.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class PrimeFactors {
     public static List<Integer> generate(int n) {
         List<Integer> primes = new ArrayList<Integer>();
-        if (n > 1) {
+        while (n > 1) {
             if (n % 2 == 0) {
                 primes.add(2);
                 n /= 2;
