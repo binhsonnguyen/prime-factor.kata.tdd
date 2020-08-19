51b591d2fb766292c6e12cf0a4b77be6d6630a0a
edward <son@binhsonnguyen.com>
2020-08-19 12:04:02 +0700

    rearrange code

--- src/test/java/math/primeFactor/PrimeFactorsTest.java
+++ src/test/java/math/primeFactor/PrimeFactorsTest.java
@@ -6,11 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactorsTest extends TestCase {
-    public void testOne() throws Exception {
-        assertEquals(list(), PrimeFactors.generate(1));
-    }
-
     private List<Integer> list() {
         return new ArrayList<Integer>();
+    }
+
+    public void testOne() throws Exception {
+        assertEquals(list(), PrimeFactors.generate(1));
     }
 }
