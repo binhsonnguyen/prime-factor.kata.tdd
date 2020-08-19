36b9fbb8a61c6b3f5e9b0d4e3f1254cd1c506dc6
edward <son@binhsonnguyen.com>
2020-08-19 12:25:49 +0700

    pass testTwo

--- src/main/java/math/primeFactor/PrimeFactors.java
+++ src/main/java/math/primeFactor/PrimeFactors.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
-    public static List<Integer> generate(int number) {
-        return new ArrayList<Integer>();
+    public static List<Integer> generate(int n) {
+        List<Integer> primes = new ArrayList<Integer>();
+        if (n > 1) {
+            primes.add(2);
+        }
+        return primes;
     }
 }
