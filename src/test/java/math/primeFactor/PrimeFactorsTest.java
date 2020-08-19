faac036eb1c35c7bd617efa93b95dc6f2511d70c
edward <son@binhsonnguyen.com>
2020-08-19 11:57:47 +0700

    `list` signature

--- src/test/java/math/primeFactor/PrimeFactorsTest.java
+++ src/test/java/math/primeFactor/PrimeFactorsTest.java
@@ -2,8 +2,14 @@ package math.primeFactor;
 
 import junit.framework.TestCase;
 
+import java.util.List;
+
 public class PrimeFactorsTest extends TestCase {
     public void testOne() throws Exception {
         assertEquals(list(), PrimeFactors.generate(1));
+    }
+
+    private List<Integer> list() {
+        return null;
     }
 }
